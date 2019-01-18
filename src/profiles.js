54000bbc0c6921307cc6035931c5cce1a3edb04d
Andreas Hagman <Andreas.Hagman@enfogroup.com>
2019-01-18 13:39:46 +0100

    Update

--- src/profiles.js
+++ src/profiles.js
@@ -1,7 +1,24 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const shell = require('shelljs');
+const homeDir = process.env['HOME']
+const profileRegex = /\[.*]/g;
+const bracketsRemovalRegx = /(\[| )|(\])/g;
 
 exports.prompt = async () => {
-    const profiles = getProfiles();
+    const data = await getProfiles();
+
+    const matches = data.match(profileRegex);
+
+    if (!matches) {
+        console.log('No profiles found.');
+        return;
+    }
+
+    const profiles = matches.map((match) => {
+        return match.replace(bracketsRemovalRegx, '');
+    });
+
 
     const questions = [
         {
@@ -16,10 +33,31 @@ exports.prompt = async () => {
     await changeProfile(answer);
 };
 
-const changeProfile = async ({ PROFILE }) => {
-    console.log(`Changed to ${PROFILE}..`);
+const changeProfile = ({ PROFILE }) => {
+    console.log(PROFILE)
+    // aws_access_key_id,  aws_secret_access_key, aws_session_token, region
+    // check if MFA
+}
+
+const setProfile = async ({ aws_access_key_id,  aws_secret_access_key, aws_session_token, region  }) => {
+    shell.exec('aws configure set aws_access_key_id ' + aws_access_key_id);
+    shell.exec('aws configure set aws_secret_access_key ' + aws_secret_access_key)
+    shell.exec('aws configure set aws_session_token ' + aws_session_token)
+    shell.exec('aws configure set region ' + region || 'eu-west-1');  
+}
+
+const exportEnvironment = () => {
+    // Export key id, session token and sercret key.
 }
 
 const getProfiles = () => {
-    return ['Profile-1', 'Profile-2']
-}
+    return new Promise((resolve, reject) => {
+        fs.readFile(`${homeDir}/.aws/credentials`, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
